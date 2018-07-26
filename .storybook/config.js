84b8bcec902d360304eb7bc0a04f60581bc3e198
Iiro Jäppinen <iiro@jappinen.fi>
2018-07-26 13:27:29 +0300

    Add polyfill

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import '@babel/register';
+import '@babel/polyfill'
+
 import { configure } from '@storybook/react';
 
 const requireAll = requireContext => requireContext.keys().map(requireContext);
