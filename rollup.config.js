b6f06cace301f2e83793317053688726a0cd119a
Iiro Jäppinen <iiro@jappinen.fi>
2019-01-03 20:30:48 +0200

    feat: build umd bundle in addition to commonjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,43 @@
-import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
 import minify from "rollup-plugin-babel-minify";
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: "src/index.ts",
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies)
+];
 
-  output: [
-    {
-      exports: "named",
-      file: pkg.main,
-      format: "cjs"
+const plugins = [
+  typescript({
+    tsconfig: "tsconfig.build.json",
+    typescript: require("typescript")
+  }),
+  production && minify({ comments: false })
+];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      { exports: "named", file: pkg.main, format: "cjs" },
+      { exports: "named", file: pkg.module, format: "es" }
+    ],
+    external,
+    plugins
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.browser,
+      format: "umd",
+      name: "styledModal"
     },
-    {
-      exports: "named",
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
-
-  plugins: [
-    typescript({
-      tsconfig: "tsconfig.build.json",
-      typescript: require("typescript")
-    }),
-    production &&
-      minify({
-        comments: false
-      })
-  ]
-};
+    external,
+    plugins: [...plugins, resolve(), commonjs()]
+  }
+];
