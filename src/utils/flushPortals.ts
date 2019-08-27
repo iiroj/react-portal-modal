e256f0c889bb64e5bd08806a4940a54eeaff77ba
Iiro Jäppinen <iiro@jappinen.fi>
2019-08-27 19:27:45 +0300

    refactor: fix eslint issues

--- src/utils/flushPortals.ts
+++ src/utils/flushPortals.ts
@@ -1,6 +1,6 @@
 import { hasDom } from "./hasDom";
 
-export function flushPortals(target: string = "modal") {
+export function flushPortals(target = "modal") {
   if (hasDom()) {
     const portals = document.getElementById(target);
     // eslint-disable-next-line no-unmodified-loop-condition
